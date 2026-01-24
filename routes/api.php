88a7014564c69ac0372d3c1d2b0c60efb0228e63
Sufian Diraneyya <sufian@diraneyya.com>
2026-01-24 17:30:10 +0300

    Add api routing definitions

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\ClassroomController;
+use App\Http\Controllers\ClassroomTeacherController;
+use App\Http\Controllers\ClassroomStudentsController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('students', StudentController::class);
+    Route::apiResource('classrooms', ClassroomController::class);
+
+    Route::prefix('classrooms/{classroom}')->group(function () {
+        Route::get('teacher', [ClassroomTeacherController::class, 'show']);
+        Route::put('teacher', [ClassroomTeacherController::class, 'assign']);
+        Route::delete('teacher', [ClassroomTeacherController::class, 'unassign']);
+
+        Route::get('students', [ClassroomStudentsController::class, 'index']);
+        Route::post('students', [ClassroomStudentsController::class, 'assign']);
+        Route::delete('students/{student}', [ClassroomStudentsController::class, 'unassign']);
+    });
+});
